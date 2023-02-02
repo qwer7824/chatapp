b295a36367fd2e186f3c4a2e869decbd65743b52
qwer7824 <sh7824@naver.com>
2023-02-03 01:41:50 +0900

    2/2

---
+++ chatapp/src/main/java/com/cos/chatapp/ChatController.java
@@ -0,0 +1,40 @@
+package com.cos.chatapp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController // 데이터 리턴 서버
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    //귓속말 할때 사용
+    @CrossOrigin
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender,@PathVariable String receiver){
+        return chatRepository.mFindBySender(sender,receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum){
+        return chatRepository.mFindByRoomNum(roomNum)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat){
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat); // Object를 리턴하면 자동으로 json 변환 (messageConverter)
+    }
+
+}
